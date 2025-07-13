d87f03a2aedcf6436de8e608cc8dc8acb953b195
ahaoboy <504595380@qq.com>
2025-07-13 16:34:04 +0800

    test

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,22 @@
-#[cfg(windows)]
-#[link(name = "shell32")]
-unsafe extern "system" {
-    fn IsUserAnAdmin() -> bool;
-}
+// #[cfg(windows)]
+// #[link(name = "shell32")]
+// unsafe extern "system" {
+//     fn IsUserAnAdmin() -> bool;
+// }
+
+// #[cfg(windows)]
+// pub fn is_admin() -> bool {
+//     unsafe { IsUserAnAdmin() }
+// }
 
 #[cfg(windows)]
 pub fn is_admin() -> bool {
-    unsafe { IsUserAnAdmin() }
+    let shell = "[bool]([System.Security.Principal.WindowsPrincipal][System.Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([System.Security.Principal.WindowsBuiltInRole]::Administrator)";
+    let output = std::process::Command::new("powershell")
+        .args(["-c", shell])
+        .output()
+        .expect("Failed to execute powershell command");
+    String::from_utf8(output.stdout).unwrap_or_default().trim() == "True"
 }
 
 #[cfg(unix)]
