975e8cd1f44fb9523822ab3fcbac20047195cc17
ahaoboy <504595380@qq.com>
2025-07-13 16:28:16 +0800

    add IsUserAnAdmin api

--- src/lib.rs
+++ src/lib.rs
@@ -10,13 +10,14 @@
 // }
 
 #[cfg(windows)]
+#[link(name = "shell32")]
+unsafe extern "system" {
+    fn IsUserAnAdmin() -> bool;
+}
+
+#[cfg(windows)]
 pub fn is_admin() -> bool {
-    let shell = "[bool]([System.Security.Principal.WindowsPrincipal][System.Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([System.Security.Principal.WindowsBuiltInRole]::Administrator)";
-    let output = std::process::Command::new("powershell")
-        .args(["-c", shell])
-        .output()
-        .expect("Failed to execute powershell command");
-    String::from_utf8(output.stdout).unwrap_or_default().trim() == "True"
+    unsafe { IsUserAnAdmin() }
 }
 
 #[cfg(unix)]
